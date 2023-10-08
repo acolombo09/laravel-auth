afcf532f93a18985b8a8ecd9684885756ec7cb90
acolombo09 <acolombo0911@gmail.com>
2023-10-08 12:26:32 +0200

    initial_commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guests.welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Raggruppo le rotte
+// Route::middleware(['auth', 'verified'])
+//     ->prefix("admin")
+//     ->name("admin.")
+//     ->group(function () {
+//     // CREATE
+//     Route::get("/posts/create", [PostController::class, "create"])->name("posts.create");
+//     Route::post("/posts", [PostController::class, "store"])->name("posts.store");
+
+//     // READ
+//     Route::get("/posts", [PostController::class, "index"])->name("posts.index");
+//     Route::get("/posts/{post}", [PostController::class, "show"])->name("posts.show");
+// });
+
+// chiamato guestpostcontroller perchè nel controller si è aggiunto "as GuestPostController"
+// per far sì che i nomi tra admin e guests non vengano confusi
+// Route::get("/posts", [GuestPostController::class, "index"])->name("posts.index");
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
+    Route::patch('/admin/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
+    Route::delete('/admin/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
+});
+
+require __DIR__.'/auth.php';
